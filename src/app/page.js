32aaa67021ba1519d1f0cd8de23b30a57e4f2385
omarjaforchy <omarjaforchy@gmail.com>
2024-09-05 16:26:11 +0600

    alert and alert dialog added

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import AlertDialogInfo from "@/components/AlertDialog";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
 import { Terminal } from "lucide-react";
@@ -11,11 +12,15 @@ export default function Home() {
       <h1 className="text-3xl font-bold text-black">Shadcn Practice Course</h1>
       
       <Button asChild>
+        <Link href="/accord">Alert</Link>
+      </Button>
+      <Button asChild>
         <Link href="/button">Button</Link>
       </Button>
       <Button asChild>
         <Link href="/accord">Accordion</Link>
       </Button>
+      <AlertDialogInfo />
     </main>
   );
 }
