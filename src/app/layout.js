e7400e2a550e0b7950e1267eed06265d7b4d3b97
omarjaforchy <omarjaforchy@gmail.com>
2024-09-05 17:16:52 +0600

    next theme dark mode implement

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +13,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,17 @@
 import AlertDialogInfo from "@/components/AlertDialog";
+import { ModeToggle } from "@/components/Theme";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
 import { Terminal } from "lucide-react";
 import Link from "next/link";
 
-
 export default function Home() {
-  
   return (
     <main className="flex flex-col items-center justify-between p-24 space-y-4">
-      <h1 className="text-3xl font-bold text-black">Shadcn Practice Course</h1>
-      
+      <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+        The People of the Kingdom
+      </h2>
+      <ModeToggle />
       <Button asChild>
         <Link href="/accord">Alert</Link>
       </Button>
